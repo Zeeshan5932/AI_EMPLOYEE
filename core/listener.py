25b52a994d05c919422a7ce35d4cc8c3707b7ad9
Zeeshan Younas <136065779+Zeeshan5932@users.noreply.github.com>
2026-02-22 16:06:24 +0500

    new

--- core/listener.py
+++ core/listener.py
@@ -1,2 +1,23 @@
+import speech_recognition as sr
+
+
 def listen_command():
-    return input("You: ")
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    try:
+        command = recognizer.recognize_google(audio)
+        print("You:", command)
+        return command.lower()
+
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+        return None
+
+    except sr.RequestError:
+        print("Speech service unavailable")
+        return None
